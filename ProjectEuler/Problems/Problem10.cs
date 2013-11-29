be95a95aab8fc13e49d996d060ca16da90898a3b
Emre Nevayeshirazi <nevayeshirazi@gmail.com>
2013-11-29 15:21:32 +0200

    Initial Commit with Problem10

---
+++ ProjectEuler/Problems/Problem10.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectEuler.Utilities;
+
+namespace ProjectEuler.Problems
+{
+    class Problem10
+    {
+        // Max natural number
+        private static const int MAX  = 2000000;
+
+        /// <summary>
+        /// Finds the sum of prime numbers less than MAX
+        /// </summary>
+        /// <returns>
+        /// Returns the calcualated the sum
+        /// </returns>
+        public static long SumOfPrimeNumbers()
+        {
+            bool[] numbers = SieveOfEratosthenes.FindPrimeNumbers(2000000);
+
+            long sum = 0;
+            for (int i = 2; i < MAX; i++)
+            {
+                if (numbers[i])
+                    sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ ProjectEuler/Utilities/SieveOfEratosthenes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Utilities
+{
+    class SieveOfEratosthenes
+    {
+        /// <summary>
+        /// Generates and return an array of booleans in which false values in final array are prime numbers.
+        /// </summary>
+        /// <param name="limit">
+        /// Size of the array
+        /// </param>
+        /// <returns></returns>
+        public static bool[] FindPrimeNumbers(int limit)
+        {
+            var numbers = new bool[limit];
+            numbers[0] = true;
+            numbers[1] = true;
+
+            for (int k = 2; k <= Math.Sqrt(limit); k++)
+            {
+                if (!numbers[k])
+                {
+                    for (int z = k * k; z < limit; z += k)
+                        numbers[z] = true;
+                }
+            }
+
+            return numbers;
+        }
+    }
+}
